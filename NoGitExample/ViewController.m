584be22408ee334562113a8dd649546e46e8ff6b
superbobogithub <1114026801@qq.com>
2016-03-10 17:54:26 +0800

    1111

--- NoGitExample/ViewController.m
+++ NoGitExample/ViewController.m
@@ -9,19 +9,20 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic) int sum;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    int a = 5;
+    int b = 10;
+    
+    self.sum = a + b;
+    
+    NSLog(@"The result is: %d", self.sum);
 }
 
 @end
