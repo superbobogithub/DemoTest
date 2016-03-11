46e676bc8f14e8315f5af81337b59726e45d683f
superbobogithub <1114026801@qq.com>
2016-03-11 11:02:40 +0800

    提交添加文件

---
+++ NoGitExample/TestClass.h
@@ -0,0 +1,16 @@
+//
+//  TestClass.h
+//  NoGitExample
+//
+//  Created by SGB on 16/3/11.
+//  Copyright © 2016年 索马里猫集团股份有限公司. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@property (nonatomic) int sum;
+@property (nonatomic, strong) TestClass *testClass;
+
+@end
---
+++ NoGitExample/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  NoGitExample
+//
+//  Created by SGB on 16/3/11.
+//  Copyright © 2016年 索马里猫集团股份有限公司. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
